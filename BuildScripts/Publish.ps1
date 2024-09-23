903f759899e2e902e739e131094dd3c974db83ff
Ara Petrosyan <ara.petrosyan@hotmail.com>
2024-09-23 22:46:30 +0400

    Created github release publishing script

---
+++ BuildScripts/Publish.ps1
@@ -0,0 +1,20 @@
+﻿# Run in release mode
+param($Configuration = "Release")
+
+# Get the original directoty, it will get back there after it's done
+$OriginalDir=Get-Location | select -ExpandProperty Path
+
+# Go to the project directory
+cd ../Projects/Utils
+
+# Tagret path of published artifacts
+$TargetPath = "../../bin/$Configuration/publish/Utils"
+
+# Publish artifacts
+dotnet publish -c $Configuration -o $TargetPath
+
+# Zip the contents
+Compress-Archive -Path $TargetPath -DestinationPath $TargetPath\Armat.Utils-1.0.1.zip -Force
+
+# Go back to the original directory
+cd $OriginalDir
