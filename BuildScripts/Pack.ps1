66766e919cb1ae996d40d91bc9e2832b1625d66e
Ara Petrosyan <ara.petrosyan@hotmail.com>
2024-09-23 23:25:18 +0400

    Created Nuget Package generation script

---
+++ BuildScripts/Pack.ps1
@@ -0,0 +1,17 @@
+﻿# Run in release mode
+param($Configuration = "Release")
+
+# Get the original directoty, it will get back there after it's done
+$OriginalDir=Get-Location | select -ExpandProperty Path
+
+# Go to the project directory
+cd ../Projects/Utils
+
+#Build the project
+dotnet build Utils.csproj -c $Configuration -o "$OriginalDir/../bin/$Configuration"
+
+# Pack nuget artifacts
+dotnet pack Utils.csproj -c $Configuration --no-build -o "$OriginalDir/../bin/$Configuration" /p:OutputPath="$OriginalDir/../bin/$Configuration"
+
+# Go back to the original directory
+cd $OriginalDir
